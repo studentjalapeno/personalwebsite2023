35f2b366066e6df8e2fb755caa7adaeaac26690c
studentjalapeno <60950099+studentjalapeno@users.noreply.github.com>
2023-11-02 17:48:31 -0500

    Fixing home options on mobile

--- src/app/home-options/home-options.data.ts
+++ src/app/home-options/home-options.data.ts
@@ -2,11 +2,9 @@ export const homeOptionsData = [
   [
     { id: 'about', label: 'About', iconSrc: 'assets/question-block-red.gif', iconAlt: 'About Spinning Icon' },
     { id: 'services', label: 'Services', iconSrc: 'assets/github-logo.gif', iconAlt: 'Github Spinning Icon' },
-    { id: 'projects', label: 'Projects', iconSrc: 'assets/projects_icon.gif', iconAlt: 'Projects Animated Icon' },
   ],
   [
-    { id: 'resume', label: 'Resume', iconSrc: 'assets/resume_icon.gif', iconAlt: 'Resume Spinning Icon' },
-    { id: 'blog', label: 'Blog', iconSrc: 'assets/blog_icon.gif', iconAlt: 'Blog Spinning Icon' },
+    { id: 'projects', label: 'Projects', iconSrc: 'assets/projects_icon.gif', iconAlt: 'Projects Animated Icon' },
     { id: 'contact', label: 'Contact', iconSrc: 'assets/email-spin-animated.gif', iconAlt: 'About Spinning Icon' },
   ],
 ];
